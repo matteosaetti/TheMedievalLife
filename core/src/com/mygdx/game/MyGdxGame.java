b605cb8ae4540e339f033b4fc46c3fe6a7073338
matteosaetti <matteosaett@gmail.com>
2021-10-20 11:28:50 +0200

    black bg

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -17,7 +18,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+		Gdx.gl20.glClearColor(0,0,0,1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
