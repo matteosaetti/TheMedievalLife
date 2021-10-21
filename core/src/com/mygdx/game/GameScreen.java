c0883e1144ee5596bbf2085f5b66116e3f6f13b2
matteosaetti <matteosaett@gmail.com>
2021-10-21 08:26:14 +0200

    player's movement

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -20,13 +20,28 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
 public class GameScreen implements Screen {
     final MyGdxGame game;
+    Rectangle player;
+    Texture playerImage;
     OrthographicCamera camera;
     public GameScreen(final MyGdxGame game) {
         this.game = game;
 
+        playerImage = new Texture(Gdx.files.internal("bucket.png"));
         // create the camera and the SpriteBatch
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
+        camera.position.x=400;
+        camera.position.y=240;
+        camera.update();
+        //create player
+        player = new Rectangle();
+        player.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+        player.y = 20; // bottom left corner of the bucket is 20 pixels above
+        // the bottom screen edge
+        player.width = 32;
+        player.height = 32;
+
+
     }
 
     @Override
@@ -36,13 +51,56 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        // clear the screen with a dark blue color. The
+        // arguments to clear are the red, green
+        // blue and alpha component in the range [0,1]
+        // of the color to be used to clear the screen.
         ScreenUtils.clear(0, 0, 0.2f, 1);
+
         // tell the camera to update its matrices.
         camera.update();
 
         // tell the SpriteBatch to render in the
         // coordinate system specified by the camera.
         game.batch.setProjectionMatrix(camera.combined);
+
+        // begin a new batch and draw the bucket and
+        // all drops
+
+
+        // process user input
+        if (Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            player.x = touchPos.x - 64 / 2;
+        }
+
+        game.batch.begin();
+
+        game.batch.draw(playerImage, player.x, player.y, player.width, player.height);
+
+        game.batch.end();
+
+        if (Gdx.input.isKeyPressed(Keys.A))
+            player.x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Keys.D))
+            player.x += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Keys.W))
+            player.y += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Keys.S))
+            player.y -= 200 * Gdx.graphics.getDeltaTime();
+
+        if (player.x < 0)
+            player.x = 0;
+        if (player.x > 800 - 64)
+            player.x = 800 - 64;
+        if (player.y > 480 - 64)
+            player.y = 480 - 64;
+        if (player.y < 0)
+            player.y = 0;
+
+
     }
 
     @Override
@@ -67,6 +125,6 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        playerImage.dispose();
     }
 }
